58f04329f2d3bca921cda00e101f7a8f7eaf538a
dhanu-1508 <55988777+dhanu-1508@users.noreply.github.com>
2021-04-23 18:20:23 +0530

    Add files via upload

---
+++ Assignment1/Assignment1/DemoFactEven.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class DemoFactEven
+    {
+
+        public int  n, fact = 1,num;
+        public void factorial()
+        {
+            for(int j=1;j<=n;j++)
+            {
+                fact = fact * j;
+            }
+            Console.WriteLine("factorial of a number is " + fact);
+            Console.ReadLine();
+        }
+
+       
+
+        public void even()
+        {
+            if(n%2==0)
+            {
+                Console.WriteLine("the number is even");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("number is odd");
+                Console.ReadLine();
+            }
+        }
+        public void DemoWhile()
+        {
+            int i = 1;
+            Console.WriteLine("table of 5 is");
+            while (i <= 10)
+            {
+                num = n * i;
+                Console.WriteLine("" + num);
+                i++;
+            }
+          
+            Console.ReadLine();
+        }
+        public void DemoDoWhile()
+
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+               
+            } while (i < 6);
+            Console.ReadLine();
+           
+           
+        }
+       
+    }
+}
---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+       // int ch;
+        static void Main(string[] args)
+        {
+            DemoFactEven df = new DemoFactEven();
+          
+            // ch chi value kuthun ghety?
+            //input wali line 
+            Console.WriteLine("enter the choice");
+          int ch= Convert.ToInt32 (Console.ReadLine());
+            //just a sec asach asu de mi baghto majya side la
+            // i think type casting karav lgel mala nai watat tham,b bhgtoy
+            // nhi hot he
+            //enjoy wait run kru de
+            // while ani do while ch output nhi yet heit wait baghto bachu
+
+            switch (ch)
+            {
+                case 1:
+                    df.n = 5;
+
+                    df.factorial();
+                    break;
+                case 2:
+                    df.n = 5;
+                    df.even();
+                    break;
+                case 3:
+                    df.n = 5;
+                    df.DemoWhile();
+                    break;
+                case 4:
+                    df.DemoDoWhile();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
